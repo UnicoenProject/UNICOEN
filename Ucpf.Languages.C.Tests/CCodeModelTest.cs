5e0386cee298a1401a131624e285cf2d2a9260ac
t3kot3ko <kamiya7140@gmail.com>
2010-11-26 15:42:15 +0900

    Modify CCodeModelTest.cs and namespaces.

---
+++ Ucpf.Languages.C.Tests/CCodeModelTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Ucpf.Languages.C.Tests
+{
+	public class CCodeModelTest
+	{
+		[Test]
+		public void CFunctionを生成できる()
+		{
+			var ast = CAstGeneratorOld.Instance.GenerateFromFile("fibonacci.c");
+			var root = ast.Descendants("function_definition").First();
+			var func = new CFunction(root);
+			Assert.That(func.Name, Is.EqualTo("fibonacci"));
+		}
+
+		[Test]
+		public void パラメータリストが正しい()
+		{
+			var ast = CAstGeneratorOld.Instance.GenerateFromFile("fibonacci.c");
+			var root = ast.Descendants("function_definition").First();
+			var func = new CFunction(root);
+			Assert.That(func.Arguments.ElementAt(0).Name, Is.EqualTo("n"));
+			Assert.That(func.Arguments.ElementAt(0).Type.Name, Is.EqualTo("int"));
+		}
+
+	}
+}
