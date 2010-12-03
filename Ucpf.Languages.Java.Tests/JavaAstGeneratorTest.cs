b2a89c3c1729539f2524fe2c66f132e46bb579b4
KAZUu <exkazuu@gmail.com>
2010-12-03 17:23:38 +0900

    Add a test case.

---
+++ Ucpf.Languages.Java.Tests/JavaAstGeneratorTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Ucpf.Languages.Java.Tests
+{
+	public class JavaAstGeneratorTest
+	{
+		[Test]
+		public void ユニコード文字の入ったコードをパースできる() {
+			JavaAstGenerator.Instance.GenerateFromFile("../fixture/Unicode.java");
+		}
+	}
+}
---
+++ fixture/Unicode.java
@@ -0,0 +1,5 @@
+public class Unicode {
+	public static void main(String[] args) {
+		System.out.println("\ua041\uab41\uad91\u0001");
+	}
+}
