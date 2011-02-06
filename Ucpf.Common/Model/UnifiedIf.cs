6c1508ef34815ca71ae70971326d9394e4f69f81
irxground <irxnjhtchlnrw+github@gmail.com>
2011-02-06 18:13:43 +0900

    change IfStatement -> If

---
+++ Ucpf.Common/Model/UnifiedIf.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ucpf.Common.Model
+{
+    public class UnifiedIf : UnifiedExpression
+    {
+		public UnifiedExpression Condition { get; set; }
+    	public UnifiedBlock TrueBlock { get; set; }
+		public UnifiedBlock FalseBlock { get; set; }
+    }
+}
