e7be2acbc8423d896d8288e7b9d3ada4c15054a9
irxground <irxnjhtchlnrw+github@gmail.com>
2011-02-06 17:50:25 +0900

    フィボナッチを作成中

---
+++ Ucpf.Languages.Python2.Tests/PythonFibonacciTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Ucpf.Languages.Python2.Tests {
+
+	[TestFixture]
+	class PythonFibonacciTest {
+
+		[Test]
+		void TestFibonacci() {
+			var code = @"
+def fib(n):
+  if n <= 1:
+    return n:
+  else
+    return fib(n-1) + fib(n-2)
+";
+
+		}
+	}
+}
