0d1605c84ae01589bfb0fb89b8ac48592cb6b4f0
irxground <irxnjhtchlnrw+github@gmail.com>
2011-02-06 17:21:47 +0900

    create test and pass

---
+++ Ucpf.Languages.Python2/Model/PythonModelFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Linq;
+using Ucpf.Common.Model;
+
+namespace Ucpf.Languages.Python2.Model {
+
+	public class PythonModelFactory {
+
+		public static UnifiedStringLiteral CreateStringLiteral(XElement ast) {
+			return new UnifiedStringLiteral {
+				Value = "1"
+			};
+		}
+	}
+}
