fae5c4925b142b80cea71248dfac1821f479a139
irxground <irxnjhtchlnrw+github@gmail.com>
2011-02-14 16:49:52 +0900

    fix fibonacci code

--- Ucpf.Languages.Python2.Tests/PythonFibonacciTest.cs
+++ Ucpf.Languages.Python2.Tests/PythonFibonacciTest.cs
@@ -16,10 +16,9 @@ namespace Ucpf.Languages.Python2.Tests {
 		private const string TestCode = @"
 def fib(n):
   if n <= 1:
-	return n:
-  else
+	return n
+  else:
 	return fib(n-1) + fib(n-2)
-
 print fib(20)
 ";
 
