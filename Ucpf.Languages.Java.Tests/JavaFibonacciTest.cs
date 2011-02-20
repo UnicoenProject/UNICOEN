e078cc706d8323ed36516623d4d52ad7b021c559
ganatcs <a.sea.of.deceits@gmail.com>
2011-02-20 18:37:42 +0900

    forget 'git add'...

---
+++ Ucpf.Languages.Java.Tests/JavaFibonacciTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Ucpf.Common.Model;
+using Ucpf.Common.Tests;
+using Ucpf.Languages.Java.AstGenerators;
+using Ucpf.Languages.Java.CodeModel;
+
+namespace Ucpf.Languages.Java.Tests
+{
+	[TestFixture]
+	public class JavaFibonacciTest {
+		/*
+		private static UnifiedCall CreateCall(int? decrement) {
+			return new UnifiedCall {
+				Function = new UnifiedVariable("fibonacci"),
+				Arguments = new UnifiedArgumentCollection {
+					decrement == null
+						? (UnifiedArgument)new UnifiedVariable("n")
+						: (UnifiedArgument)new UnifiedBinaryExpression {
+							LeftHandSide = new UnifiedVariable("n"),
+							Operator =
+						  	new UnifiedBinaryOperator("-", BinaryOperatorType.Subtraction),
+							RightHandSide = new UnifiedIntegerLiteral((int)decrement),
+						}
+				},
+			};
+		 */
+		}
+}
