a914443ca5818d6eab04c5a73a18e48678d14970
ganatcs <a.sea.of.deceits@gmail.com>
2011-02-22 17:25:14 +0900

    change implicit cast to ToStatement method close #35

--- Ucpf.Common/Model/Expressions/UnifiedExpression.cs
+++ Ucpf.Common/Model/Expressions/UnifiedExpression.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Ucpf.Common.Model
-{
-	public abstract class UnifiedExpression : UnifiedElement
-	{
-		public static implicit operator UnifiedStatement(UnifiedExpression expr) {
-			return new UnifiedExpressionStatement(expr);
+namespace Ucpf.Common.Model {
+	public abstract class UnifiedExpression : UnifiedElement {
+		public UnifiedStatement ToStatement() {
+			return new UnifiedExpressionStatement(this);
 		}
 	}
 }
