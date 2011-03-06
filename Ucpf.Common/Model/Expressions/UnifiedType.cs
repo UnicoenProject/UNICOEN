d2d26b0d4010ba761258b051224d231ab47bc9b2
Kazunori SAKAMOTO <exkazuu@gmail.com>
2011-03-06 21:28:25 +0900

    Fix UnifiedType to be derived from UnifiedElement.

--- Ucpf.Common/Model/Expressions/UnifiedType.cs
+++ Ucpf.Common/Model/Expressions/UnifiedType.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Ucpf.Common.Model.Visitors;
 
 namespace Ucpf.Common.Model {
-	public class UnifiedType {
+	public class UnifiedType : UnifiedElement {
 		public string Name { get; set; }
 
 		public static UnifiedType Create(string name) {
 			return new UnifiedType {
 				Name = name,
 			};
+		}
+
+		public override void Accept(IUnifiedModelVisitor visitor) {
+			throw new NotImplementedException();
 		}
 	}
 }
