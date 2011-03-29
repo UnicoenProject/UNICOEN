f27dcf40f90cc807a1acb9c97ea9681b23644c8a
ganatcs <a.sea.of.deceits@gmail.com>
2011-03-29 17:37:38 +0900

    pass CreateFor test of Java SpecificationTest.cs

--- Ucpf.Languages.Java/Model/JavaModelFactory.cs
+++ Ucpf.Languages.Java/Model/JavaModelFactory.cs
@@ -363,7 +363,7 @@ namespace Ucpf.Languages.Java.Model {
 				var step = expressionList != null
 							? CreateExpressionList(expressionList) : null;
 
-				var body = CreateStatement(forstatement);
+				var body = CreateStatement(forstatement.Element("statement"));
 				return new UnifiedFor {
 					Initializer = initializer,
 					Condition = condition,
@@ -409,6 +409,9 @@ namespace Ucpf.Languages.Java.Model {
 		}
 
 		public static UnifiedExpression CreateForInit(XElement node) {
+			Contract.Requires(node.Name.LocalName == "forInit");
+		/* forInit : localVariableDeclaration | expressionList ;
+		 */
 			switch(node.FirstElement().Name.LocalName) {
 				case "localVariableDeclaration":
 					return CreateLocalVariableDeclaration(node.FirstElement());
@@ -417,24 +420,13 @@ namespace Ucpf.Languages.Java.Model {
 			}
 			throw new InvalidOperationException();
 		}
-		
-		private static UnifiedExpression CreateLocalVariableDeclaration(XElement xElement) {
-			/*
-			 * localVariableDeclaration 
-				:   variableModifiers type variableDeclarator (',' variableDeclarator )* ;
-			 */
-			return CreateExpression(xElement);
-			//TODO: 構文に沿ったように実装する
-			//var variables = new List<UnifiedExpression>();
-			//return new UnifiedExpressionCollection(variables);
 
-		}
-
-		private static UnifiedModifier CreateVariableModifiers(XElement xElement) {
+		private static UnifiedModifierCollection CreateVariableModifiers(XElement xElement) {
 			/*
 			 * variableModifiers : ( 'final' | annotation )* ;
 			 */
-			return new UnifiedModifier();
+			if (xElement.Elements().Count() == 0) return null;
+			else throw new NotImplementedException();
 		}
 
 		public static UnifiedExpression CreateExpressionList(XElement node) {
@@ -555,18 +547,26 @@ namespace Ucpf.Languages.Java.Model {
 
 		public static UnifiedVariableDefinition CreateVariableDefinition(XElement node) {
 			//Top node is <localVariableDeclarationStatement>.
-			var variableDeclaration = node.Element("localVariableDeclaration");
+			//var variableDeclaration = node.Element("localVariableDeclaration");
+			return CreateLocalVariableDeclaration(node.Element("localVariableDeclaration"));
+
+		}
+		public static UnifiedVariableDefinition CreateLocalVariableDeclaration(XElement node) {
+			Contract.Requires(node.Name.LocalName == "localVariableDeclaration");
+			/* localVariableDeclaration 
+			 *   :   variableModifiers type variableDeclarator (',' variableDeclarator )* ;
+			 */
 			return new UnifiedVariableDefinition {
 					InitialValue = CreateExpression(
-						variableDeclaration.Element("variableDeclarator")
+						node.Element("variableDeclarator")
 						.Element("variableInitializer")
 						.Element("expression")),
-					Modifiers = new UnifiedModifierCollection(variableDeclaration
+					Modifiers = new UnifiedModifierCollection(node
 						.Element("variableModifiers")
 						.Elements()
 						.Select(CreateVariableModifier)),
-					Name = variableDeclaration.Element("variableDeclarator").Element("IDENTIFIER").Value,
-					Type = CreateType(variableDeclaration)
+					Name = node.Element("variableDeclarator").Element("IDENTIFIER").Value,
+					Type = CreateType(node)
 			};
 		}
 
