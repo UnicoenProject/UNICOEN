aa66a2f95ea84ae08860696e78cbe48b981b245e
GreatAS <akira.radiant@gmail.com>
2011-04-05 23:30:09 +0900

    Developing...

--- Ucpf.Core/Model/UnifiedElement.cs
+++ Ucpf.Core/Model/UnifiedElement.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using Ucpf.Core.Model.Visitors;
