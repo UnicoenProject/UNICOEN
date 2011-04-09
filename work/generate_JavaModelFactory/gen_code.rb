adbd568e3fcaea55e768ca963c95bff417a39ac1
Kazunori SAKAMOTO <exkazuu@gmail.com>
2011-04-10 06:35:59 +0900

    Add scripts.

---
+++ work/generate_JavaModelFactory/gen_code.rb
@@ -0,0 +1,13 @@
+methods = open('java.g', 'r') { |f| f.readlines.map { |s| s.strip } }
+
+source = methods.map { |m|
+%Q{
+        public static IUnifiedElement Create#{m[0].upcase + m[1..-1]}(XElement node) {
+            Contract.Requires(node != null);
+            Contract.Requires(node.Name() == "#{m}");
+            return null;
+        }
+}
+}.join
+
+open('out.txt', 'w') { |f| f.write source }
