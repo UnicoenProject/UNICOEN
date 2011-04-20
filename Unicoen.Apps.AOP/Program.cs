ea46c5ed770621bfdf2bb97f5fe0aab02ed8fdf7
GreatAS <akira.radiant@gmail.com>
2011-04-20 16:50:21 +0900

    add AOP project and Tests

---
+++ Unicoen.Apps.AOP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Paraiba.Text;
+using Unicoen.Core.Model;
+using Unicoen.Languages.CSharp;
+using Unicoen.Languages.Java.ModelFactories;
+
+namespace Unicoen.Apps.AOP {
+	class Program {
+
+		private static UnifiedProgram CreateModel(string ext, string code) {
+			switch (ext.ToLower()) {
+			case ".cs":
+				return CSharpModelFactory.CreateModel(code);
+			case ".java":
+				return JavaModelFactoryHelper.CreateModel(code);
+			}
+			return null;
+		}
+
+		private static void Main(string[] args) {
+			/* parameters
+			 *  :  args[0] -> filePath
+			 */
+			//var filePath = args[0];
+			const string filePath =
+					@"C:\Users\GreatAS\Desktop\Unicoen\fixture\Java\input\default\Block1.java";
+
+			var ext = Path.GetExtension(filePath);
+			var code = File.ReadAllText(filePath, XEncoding.SJIS);
+			var model = CreateModel(ext, code);
+
+			foreach (var e in model.Descendants()) {
+				if(e is UnifiedBinaryExpression)
+					Console.Write(e.ToString());
+			}
+		}
+	}
+}
