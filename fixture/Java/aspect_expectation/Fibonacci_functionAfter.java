dece39064e387979da908e14f8d96fc3e6556030
GreatAS <akira.radiant@gmail.com>
2011-04-26 15:00:29 +0900

    Refactoring of AOP-Apps tests.

---
+++ fixture/Java/aspect_expectation/Fibonacci_functionAfter.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+	public static int fibonacci(int n) {
+		if (n < 2) {
+			{ Console.Write(); }
+			return n;
+		}
+		else {
+			{ Console.Write(); }
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
+	}
+}
+
