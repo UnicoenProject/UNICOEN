a967528f5914a5f5214e7caf3acf1da61804617f
GreatAS <akira.radiant@gmail.com>
2011-04-26 17:53:31 +0900

    Cleanup JoinPoint model and modify code.

--- Unicoen.Apps.AOP.Tests/PointcutTest.cs
+++ Unicoen.Apps.AOP.Tests/PointcutTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 using Paraiba.Text;
 using Unicoen.Core.Model;
@@ -26,52 +27,166 @@ namespace Unicoen.Apps.AOP.Tests {
 		}
 
 		[Test]
-		public void WeavingAtFunctionBeforeCorrectly() {
+		public void WeavingAtBeforeExecutionAll() {
 			var model = CreateModel(FibonacciPath);
 			var actual =
 					CreateModel(
 							@"C:\Users\GreatAS\Desktop\Unicoen\fixture\Java\aspect_expectation\Fibonacci_functionBefore.java");
 			
-			CodeProcessor.InsertBeforeAllFunction(model, "{Console.Write();}");
+			CodeProcessor.InsertAtBeforeExecutionAll(model, "{Console.Write();}");
 
 			//TODO ToString()しないと比較できないか
 			Assert.That(model.ToString(), Is.EqualTo(actual.ToString()));
 		}
 
 		[Test]
-		public void WeavingAtFunctionAfterCorrectly() {
+		public void WeavingAtAfterExecutionAll() {
 			var model = CreateModel(FibonacciPath);
 			var actual =
 					CreateModel(
 							@"C:\Users\GreatAS\Desktop\Unicoen\fixture\Java\aspect_expectation\Fibonacci_functionAfter.java");
 			
-			CodeProcessor.InsertAfterAllFunction(model, "{Console.Write();}");
+			CodeProcessor.InsertAtAfterExecutionAll(model, "{Console.Write();}");
 
 			//TODO ToString()しないと比較できないか
 			Assert.That(model.ToString(), Is.EqualTo(actual.ToString()));
 		}
 
 		[Test]
-		public void WeavingAtCallBeforeCorrectly() {
+		[TestCase("^fib")]
+		public void WeavingAtBeforeExecutionByRegex(string regex) {
+			var model = CreateModel(FibonacciPath);
+			var actual =
+					CreateModel(
+							@"C:\Users\GreatAS\Desktop\Unicoen\fixture\Java\aspect_expectation\Fibonacci_functionBefore.java");
+			
+			CodeProcessor.InsertAtBeforeExecution(model, new Regex(regex), "{Console.Write();}");
+
+			//TODO ToString()しないと比較できないか
+			Assert.That(model.ToString(), Is.EqualTo(actual.ToString()));
+		}
+
+		[Test]
+		[TestCase("^fib")]
+		public void WeavingAtAfterExecutionByRegex(string regex) {
+			var model = CreateModel(FibonacciPath);
+			var actual =
+					CreateModel(
+							@"C:\Users\GreatAS\Desktop\Unicoen\fixture\Java\aspect_expectation\Fibonacci_functionAfter.java");
+			
+			CodeProcessor.InsertAtAfterExecution(model, new Regex(regex), "{Console.Write();}");
+
+			//TODO ToString()しないと比較できないか
+			Assert.That(model.ToString(), Is.EqualTo(actual.ToString()));
+		}
+
+		[Test]
+		[TestCase("fibonacci")]
+		public void WeavingAtBeforeExecutionByName(string name) {
+			var model = CreateModel(FibonacciPath);
+			var actual =
+					CreateModel(
+							@"C:\Users\GreatAS\Desktop\Unicoen\fixture\Java\aspect_expectation\Fibonacci_functionBefore.java");
+			
+			CodeProcessor.InsertAtBeforeExecutionByName(model, name, "{Console.Write();}");
+
+			//TODO ToString()しないと比較できないか
+			Assert.That(model.ToString(), Is.EqualTo(actual.ToString()));
+		}
+
+		[Test]
+		[TestCase("fibonacci")]
+		public void WeavingAtAfterExecutionByName(string name) {
+			var model = CreateModel(FibonacciPath);
+			var actual =
+					CreateModel(
+							@"C:\Users\GreatAS\Desktop\Unicoen\fixture\Java\aspect_expectation\Fibonacci_functionAfter.java");
+			
+			CodeProcessor.InsertAtAfterExecutionByName(model, name, "{Console.Write();}");
+
+			//TODO ToString()しないと比較できないか
+			Assert.That(model.ToString(), Is.EqualTo(actual.ToString()));
+		}
+
+
+		[Test]
+		public void WeavingAtBeforeCallAll() {
 			var model = CreateModel(StudentPath);
 			var actual =
 					CreateModel(
 							@"C:\Users\GreatAS\Desktop\Unicoen\fixture\Java\aspect_expectation\Student_callBefore.java");
 
-			CodeProcessor.InsertBeforeAllCall(model, "{Console.Write();}");
+			CodeProcessor.InsertAtBeforeCallAll(model, "{Console.Write();}");
 
 			//TODO ToString()しないと比較できないか
 			Assert.That(model.ToString(), Is.EqualTo(actual.ToString()));
 		}
 
 		[Test]
-		public void WeavingAtCallAfterCorrectly() {
+		public void WeavingAtAfterCallAll() {
 			var model = CreateModel(StudentPath);
 			var actual =
 					CreateModel(
 							@"C:\Users\GreatAS\Desktop\Unicoen\fixture\Java\aspect_expectation\Student_callAfter.java");
 
-			CodeProcessor.InsertAfterAllCall(model, "{Console.Write();}");
+			CodeProcessor.InsertAtAfterCallAll(model, "{Console.Write();}");
+
+			//TODO ToString()しないと比較できないか
+			Assert.That(model.ToString(), Is.EqualTo(actual.ToString()));
+		}
+
+		//TODO 関数呼び出し(UnifiedCall)の名前の抽出が現状ではできないので、一旦無視する
+		[Test, Ignore]
+		[TestCase("^w")]
+		public void WeavingAtBeforeCallByRegex(string regex) {
+			var model = CreateModel(StudentPath);
+			var actual =
+					CreateModel(
+							@"C:\Users\GreatAS\Desktop\Unicoen\fixture\Java\aspect_expectation\Student_callBefore.java");
+
+			CodeProcessor.InsertAtBeforeCall(model, new Regex(regex), "{Console.Write();}");
+
+			//TODO ToString()しないと比較できないか
+			Assert.That(model.ToString(), Is.EqualTo(actual.ToString()));
+		}
+
+		[Test, Ignore]
+		[TestCase("^w")]
+		public void WeavingAtAfterCallByRegex(string regex) {
+			var model = CreateModel(StudentPath);
+			var actual =
+					CreateModel(
+							@"C:\Users\GreatAS\Desktop\Unicoen\fixture\Java\aspect_expectation\Student_callAfter.java");
+
+			CodeProcessor.InsertAtAfterCall(model, new Regex(regex), "{Console.Write();}");
+
+			//TODO ToString()しないと比較できないか
+			Assert.That(model.ToString(), Is.EqualTo(actual.ToString()));
+		}
+
+		[Test, Ignore]
+		[TestCase("write")]
+		public void WeavingAtBeforeCallByName(string name) {
+			var model = CreateModel(StudentPath);
+			var actual =
+					CreateModel(
+							@"C:\Users\GreatAS\Desktop\Unicoen\fixture\Java\aspect_expectation\Student_callBefore.java");
+
+			CodeProcessor.InsertAtBeforeCallByName(model, name, "{Console.Write();}");
+
+			//TODO ToString()しないと比較できないか
+			Assert.That(model.ToString(), Is.EqualTo(actual.ToString()));
+		}
+
+		[Test, Ignore]
+		[TestCase("write")]
+		public void WeavingAtAfterCallByName(string name) {
+			var model = CreateModel(StudentPath);
+			var actual =
+					CreateModel(
+							@"C:\Users\GreatAS\Desktop\Unicoen\fixture\Java\aspect_expectation\Student_callAfter.java");
+
+			CodeProcessor.InsertAtAfterCallByName(model, name, "{Console.Write();}");
 
 			//TODO ToString()しないと比較できないか
 			Assert.That(model.ToString(), Is.EqualTo(actual.ToString()));
