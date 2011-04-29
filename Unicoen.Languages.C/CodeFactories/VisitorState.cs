d54e57a2978f035ff2ac04fe8c9e825e951bcbff
t3kot3ko <kamiya7140@gmail.com>
2011-04-29 15:15:46 +0900

    Generate CCodeFactory.cs

---
+++ Unicoen.Languages.C/CodeFactories/VisitorState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Unicoen.Languages.C.CodeFactories
+{
+	public class VisitorState
+	{
+	}
+}
