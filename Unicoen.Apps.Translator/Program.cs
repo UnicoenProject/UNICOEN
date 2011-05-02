1afbe4beb3c1a363beab54741695e160493d1309
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2011-05-02 18:59:19 +0900

    Generate 'Apps.Translator' project.

---
+++ Unicoen.Apps.Translator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Unicoen.Languages.C.CodeFactories;
+using Unicoen.Languages.Java.ModelFactories;
+
+namespace Unicoen.Apps.Translator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string filePath =
+					@"C:\Users\T.Kamiya\Desktop\Unicoen\fixture\Java\input\default\Student.java";
+			var code = File.ReadAllText(filePath, Encoding.Default);
+			var model = JavaModelFactory.Instance.Generate(code);
+			var cCode = CCodeFactory.Instance.Generate(model);
+			File.WriteAllText("test.txt", cCode);
+		}
+
+		public static string GetExtention(LanguageType type)
+		{
+			switch (type)
+			{
+				case LanguageType.C:
+					return "c";
+				case LanguageType.Java:
+					return "java";
+				case LanguageType.CSharp:
+					return "cs";
+				case LanguageType.Ruby:
+					return "rb";
+				case LanguageType.Python:
+					return "py";
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+
+
+	}
+
+	public enum LanguageType
+	{
+		C,
+		Java,
+		CSharp,
+		Ruby,
+		Python,
+	}
+
+
+
+}
---
+++ Unicoen.Apps.Translator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Unicoen.Apps.Translator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Unicoen.Apps.Translator")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ee7d95c5-0ece-4b02-b2a8-f441e523e7cc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
