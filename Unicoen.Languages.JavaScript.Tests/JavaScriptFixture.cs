0937073e52921c123284f0377da9422af1a2efe3
GreatAS <akira.radiant@gmail.com>
2011-05-17 15:08:22 +0900

    Implement JavaScriptTests using RhinoJavaScriptCompiler.

--- Unicoen.Languages.JavaScript.Tests/JavaScriptFixture.cs
+++ Unicoen.Languages.JavaScript.Tests/JavaScriptFixture.cs
@@ -16,9 +16,14 @@
 
 #endregion
 
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using NUnit.Framework;
+using Paraiba.Core;
 using Unicoen.Core.CodeFactories;
 using Unicoen.Core.ModelFactories;
 using Unicoen.Core.Tests;
@@ -26,6 +31,14 @@ using Unicoen.Languages.Tests;
 
 namespace Unicoen.Languages.JavaScript.Tests {
 	public class JavaScriptFixture : Fixture {
+
+		private const string exeJavaPath = "java";
+		private readonly string[] jscCommandArguments = new[] {
+			"-cp",
+			"..\\..\\lib\\js.jar",
+			"org.mozilla.javascript.tools.jsc.Main"
+		};
+
 		/// <summary>
 		///   対応する言語のソースコードの拡張子を取得します．
 		/// </summary>
@@ -55,7 +68,7 @@ namespace Unicoen.Languages.JavaScript.Tests {
 		public override IEnumerable<TestCaseData> TestCodes {
 			get {
 				return new[] {
-						"a = 1;",
+						"var a = 1;",
 				}.Select(s => new TestCaseData(s));
 			}
 		}
@@ -69,11 +82,6 @@ namespace Unicoen.Languages.JavaScript.Tests {
 				return new[] {
 						"fibonacci",
 						"student",
-						"Block1",
-						"Block2",
-						"Block3",
-						"Sample",
-						//"Waseda"
 				}
 						.Select(
 								s =>
@@ -87,25 +95,56 @@ namespace Unicoen.Languages.JavaScript.Tests {
 		/// </summary>
 		public override IEnumerable<TestCaseData> TestDirectoryPathes {
 			get {
-				yield break;
-				//				return new[] {
-				//						new { DirName = "default", Command = "javac", Arguments = "*.java" },
-				//						new { DirName = "NewTestFiles", Command = "javac", Arguments = "*.java" },
-				//				}
-				//						.Select(
-				//								o => new TestCaseData(
-				//								     		Fixture.GetInputPath("Java", o.DirName),
-				//								     		o.Command, o.Arguments));
+				return new[] {
+						new { DirName = "Blocks", Command = "java", Arguments = "-cp ..\\..\\lib\\js.jar org.mozilla.javascript.tools.jsc.Main *.js" },
+						//new { DirName = "Waseda", Command = "java", Arguments = "*.js" },
+				}
+						.Select(
+								o => new TestCaseData(
+											FixtureUtil.GetInputPath("JavaScript", o.DirName),
+											o.Command, o.Arguments));
 			}
 		}
 
-		public override void Compile(string workPath, string fileName) {}
+		public override void Compile(string workPath, string fileName) {
+			var args = jscCommandArguments.Concat(
+					new[] {
+							"\"" + Path.Combine(workPath, fileName) + "\""
+					});
+			//e.g. (java) -cp js.jar org.mozilla.javascript.tools.jsc.Main **.js
+			var arguments = args.JoinString(" ");
+			CompileWithArguments(workPath, exeJavaPath, arguments);
+		}
 
 		public override IEnumerable<object[]> GetAllCompiledCode(string workPath) {
-			return null;
+			return Directory.EnumerateFiles(
+					workPath, "*.class",
+					SearchOption.AllDirectories)
+					.Select(path => new object[] { path, File.ReadAllBytes(path) });
 		}
 
 		public override void CompileWithArguments(
-				string workPath, string command, string arguments) {}
+				string workPath, string command, string arguments) {
+			var info = new ProcessStartInfo {
+					FileName = command, //java
+					Arguments = arguments, //-cp js.jar org.mozilla.javascript.tools.jsc.Main **.js
+					CreateNoWindow = true,
+					UseShellExecute = false,
+					WorkingDirectory = workPath,
+					RedirectStandardError = true,
+			};
+			try {
+				using (var p = Process.Start(info)) {
+					var errorMessage = p.StandardError.ReadToEnd();
+					p.WaitForExit();
+					if (p.ExitCode != 0) {
+						throw new InvalidOperationException(
+								"Failed to compile the code.\n" + errorMessage);
+					}
+				}
+			} catch (Win32Exception e) {
+				throw new InvalidOperationException("Failed to launch compiler.", e);
+			}
+		}
 	}
 }
