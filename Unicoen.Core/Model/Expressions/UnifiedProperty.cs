079b4a8de4ee73344fa1b7e2ba0d27a789e1bea9
Kazunori SAKAMOTO <exkazuu@gmail.com>
2011-06-13 03:56:58 +0900

    コメントを追加.

--- Unicoen.Core/Model/Expressions/UnifiedProperty.cs
+++ Unicoen.Core/Model/Expressions/UnifiedProperty.cs
@@ -24,10 +24,15 @@ namespace Unicoen.Core.Model {
 	///   e.g. Javaにおける<c>int a = b.c;</c>の<c>b.c</c>
 	///   e.g. Javaにおける<c>Package.ClassA a = null;</c>の<c>Package.ClassA</c>
 	///   e.g. Javaにおける<c>import Package.SubPackage;</c>の<c>Package.SubPackage</c>
+		///   e.g. Javaにおける<c>new Outer().new Inner()</c>
 	/// </summary>
 	public class UnifiedProperty : UnifiedElement, IUnifiedExpression {
 		private IUnifiedExpression _owner;
 
+		/// <summary>
+		/// アクセス元（区切り文字の左辺）を表します．
+		///   e.g. Javaにおける<c>new Outer().new Inner()</c>の<c>new Outer()</c>
+		/// </summary>
 		public IUnifiedExpression Owner {
 			get { return _owner; }
 			set { _owner = SetChild(value, _owner); }
@@ -35,11 +40,20 @@ namespace Unicoen.Core.Model {
 
 		private IUnifiedExpression _name;
 
+		/// <summary>
+		/// アクセス先（区切り文字の右辺）を表します．
+		///   e.g. Javaにおける<c>new Outer().new Inner()</c>の<c>new Inner()</c>
+		/// </summary>
 		public IUnifiedExpression Name {
 			get { return _name; }
 			set { _name = SetChild(value, _name); }
 		}
 
+		/// <summary>
+		/// 区切り文字を表します．
+		/// e.g. C++における<c>Namespace::Class</c>の<c>::</c>
+		/// e.g. Javaにおける<c>Package.Class</c>の<c>.</c>
+		/// </summary>
 		public string Delimiter { get; set; }
 
 		private UnifiedProperty() {}
