18fd87471c9cc1f345af2790d25633df9c4f72d3
Kazunori SAKAMOTO <exkazuu@gmail.com>
2011-06-24 19:58:58 +0900

    Add web application.

---
+++ Unicoen.WebApps.Demo/Controllers/MetricsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Unicoen.WebApps.Demo.ViewModels.Metrics;
+
+namespace Unicoen.WebApps.Demo.Controllers
+{
+	public partial class MetricsController : Controller
+    {
+        //
+        // GET: /Metrics/
+
+		public virtual ActionResult Index()
+        {
+            return View();
+        }
+
+		public virtual ActionResult Measure(MeasureViewModel model) {
+			// 測定して
+			model.Results = new List<MeasureResult>();
+			var java = new MeasureResult {
+					Name = "Java",
+					Values = 
+			}
+			model.Results.Add();
+
+            return View(model);
+        }
+
+    }
+}
