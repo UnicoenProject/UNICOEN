f1ff3387420aa933d6e70ec67d269e97da60d8d9
Kazunori SAKAMOTO <exkazuu@gmail.com>
2011-06-27 22:25:17 +0900

    Add support for tar.gz, tar.bzip2.

--- Unicoen.Utils/Extractor.cs
+++ Unicoen.Utils/Extractor.cs
@@ -4,8 +4,11 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using ICSharpCode.SharpZipLib.BZip2;
+using ICSharpCode.SharpZipLib.GZip;
 using ICSharpCode.SharpZipLib.Tar;
+using ICSharpCode.SharpZipLib.Zip;
 using Ionic.Zip;
+using ZipFile = Ionic.Zip.ZipFile;
 
 namespace Unicoen.Utils {
 	public static class Extractor {
@@ -14,11 +17,39 @@ namespace Unicoen.Utils {
 			Unzip(path, dirPath);
 		}
 
+		public static void Untgz(string path) {
+			var dirPath = Path.GetDirectoryName(path);
+			Untgz(path, dirPath);
+		}
+
+		public static void Untbz(string path) {
+			var dirPath = Path.GetDirectoryName(path);
+			Untbz(path, dirPath);
+		}
+
 		public static void Unzip(string inPath, string outDirPath) {
-			using (var zip = ZipFile.Read(inPath)) {
-				foreach (var e in zip) {
-					e.Extract(outDirPath, ExtractExistingFileAction.OverwriteSilently);
-				}
+			var fastZip = new FastZip();
+			fastZip.ExtractZip(inPath, outDirPath, null);
+			//using (var zip = ZipFile.Read(inPath)) {
+			//    foreach (var e in zip) {
+			//        e.Extract(outDirPath, ExtractExistingFileAction.OverwriteSilently);
+			//    }
+			//}
+		}
+
+		public static void Untgz(string inPath, string outDirPath) {
+			using (var fs = File.OpenRead(inPath))
+			using (var stream = new GZipInputStream(fs)) {
+				var archive = TarArchive.CreateInputTarArchive(stream);
+				archive.ExtractContents(outDirPath);
+			}
+		}
+
+		public static void Untbz(string inPath, string outDirPath) {
+			using (var fs = File.OpenRead(inPath))
+			using (var stream = new BZip2InputStream(fs)) {
+				var archive = TarArchive.CreateInputTarArchive(stream);
+				archive.ExtractContents(outDirPath);
 			}
 		}
 	}
