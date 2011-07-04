9879a2b8075dcbe8ad5a63d416a1847276bcd6be
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2011-07-05 01:49:37 +0900

    Temp commit

--- Unicoen.Apps/Unicoen.Apps.Translator.Tests/TestForXMLGenerator.cs
+++ Unicoen.Apps/Unicoen.Apps.Translator.Tests/TestForXMLGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
@@ -9,11 +10,27 @@ namespace Unicoen.Apps.Translator.Tests {
 	public class TestForXMLGenerator {
 		[Test]
 		public void XMLを正しく出力できる() {
-			var input = FixtureUtil.GetInputPath("Java", "default", "Hello.java");
+			var input = FixtureUtil.GetInputPath("JavaScript",  "hello.js");
 			Console.WriteLine(input);
 			var output = @"c:\";
 
 			XMLGenerator.GenerateXML(input, output);
 		}
+
+		[Test]
+		public void ファイルが無かったときに例外を吐ける() {
+			var input = FixtureUtil.GetInputPath("Java", "default", "HHello.java");
+
+			try {
+				XMLGenerator.GenerateXML(input);
+			} catch (FileNotFoundException e) {
+				Assert.True(true);
+				return;
+			}
+
+			Assert.False(true);
+
+		}
 	}
+
 }
--- Unicoen.Apps/Unicoen.Apps.Translator/Sandbox/XMLGenerator.cs
+++ Unicoen.Apps/Unicoen.Apps.Translator/Sandbox/XMLGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml;
 using Unicoen.Core.Model;
 using Unicoen.Languages.Java;
 using Unicoen.Languages.JavaScript;
@@ -12,13 +13,14 @@ namespace Unicoen.Apps.Translator {
 	/// inputFilePath の構造XMLを outputDirPath に出力するユーティリティ
 	/// </summary>
 	public class XMLGenerator {
-		public static void GenerateXML(string inputFilePath, string outputDirPath) {
-			if (!File.Exists(inputFilePath)) {
-				throw new FileNotFoundException();
-			}
-
+		/// <summary>
+		/// inputFilePath に対応する ModelGenerator を取得する
+		/// （単に拡張子で判断しているだけ）
+		/// </summary>
+		/// <param name="inputFilePath"></param>
+		/// <returns></returns>
+		private static Func<string, UnifiedProgram> GetModelGenerator(string inputFilePath) {
 			var ext = Path.GetExtension(inputFilePath);
-			Console.WriteLine(ext);
 			Func<string, UnifiedProgram> modelGenerator;
 			switch (ext) {
 				case ".java":
@@ -32,6 +34,17 @@ namespace Unicoen.Apps.Translator {
 
 			}
 
+			return modelGenerator;
+
+		}
+
+		public static void GenerateXML(string inputFilePath, string outputDirPath) {
+			if (!File.Exists(inputFilePath)) {
+				throw new FileNotFoundException();
+			}
+
+			var modelGenerator = GetModelGenerator(inputFilePath);
+
 			var model = modelGenerator(File.ReadAllText(inputFilePath));
 			var fileName = Path.GetFileName(inputFilePath);
 			var path = outputDirPath + fileName + ".xml";
@@ -41,6 +54,28 @@ namespace Unicoen.Apps.Translator {
 					writer.WriteLine(model.ToXml());
 				}
 			}
+		}
+
+		public static XmlDocument GenerateXML(string inputFilePath) {
+			if (!File.Exists(inputFilePath)) {
+				throw new FileNotFoundException();
+			}
+
+			var modelGenerator = GetModelGenerator(inputFilePath);
+
+			var model = modelGenerator(inputFilePath);
+			var xmlString = model.ToXml();
+
+			var xmlDocument = new XmlDocument();
+			var reader = new StringReader(xmlString);
+			xmlDocument.Load(reader);
+
+			return xmlDocument;
+
+		}
+
+		public static void GenerateDiffgram(string srcPath1, string srcPath2) {
+			
 		}
 	}
 }
