526ebee2436d7a8fbc8650040ba417a7e141b8e5
GreatAS <akira.radiant@gmail.com>
2011-07-16 11:20:43 +0900

    Implement GetSet Pointcut for VariableDefinition initialiser.

--- Unicoen.Apps/Unicoen.Apps.Aop.Cui.Tests/CodeProcessorTest/WeavingTest.cs
+++ Unicoen.Apps/Unicoen.Apps.Aop.Cui.Tests/CodeProcessorTest/WeavingTest.cs
@@ -339,8 +339,26 @@ namespace Unicoen.Apps.Aop.Tests {
 		}
 
 		[Test]
-		public void Getポイントカットを用いてアスペクトを合成できる() {
+		public void Getポイントカットを用いて代入文にアスペクトを合成できる() {
 			const string code = @"class A{ public void M() { int a = 10; int b; b = a; } }";
+			//モデル化
+			var model = CodeProcessor.CreateModel(".java", code);
+			var beforeNumBlock = model.Descendants().Where<UnifiedBlock>().ToCollection().Count;
+			//アスペクトの合成
+			CodeProcessor.InsertAtBeforeGet(model, new Regex("a"), CodeProcessor.CreateAdvice("Java", "System.out.println();"));
+			var afterNumBlock = model.Descendants().Where<UnifiedBlock>().ToCollection().Count;
+
+			//for debug
+			var gen = new JavaCodeFactory();
+			Console.Write(gen.Generate(model));
+
+			//For文が含まれていないので合成の前後でコードが変わらない
+			Assert.That(afterNumBlock, Is.EqualTo(beforeNumBlock + 1));
+		}
+
+		[Test]
+		public void Getポイントカットを用いて初期化子つき変数宣言にアスペクトを合成できる() {
+			const string code = @"class A{ public void M() { int a = 10; int b = a; } }";
 			//モデル化
 			var model = CodeProcessor.CreateModel(".java", code);
 			var beforeNumBlock = model.Descendants().Where<UnifiedBlock>().ToCollection().Count;
---
+++ Unicoen.Apps/Unicoen.Apps.Aop.Cui/CodeProcessor/CodeProcessorForGetSet.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Unicoen.Core.Model;
+
+namespace Unicoen.Apps.Aop
+{
+	public partial class CodeProcessor {
+				// a = b; a = 10; b;
+		// a -> set joinpoint
+		// b -> get joinpoint
+
+		//TODO アスペクトファイルではどう記述するのだろうか？
+		//普通に考えると、set : int a;のように型も指定できるのがベストなはずなので、ちょっと検討しよう
+
+		//getはBinaryExpressionの左辺以外の変数(UnifiedVariableIdentifier)？
+		//かつ、親ノードがブロックの場合とまずは少ない範囲からいきましょう
+		public static void InsertAtBeforeGet(IUnifiedElement root, Regex regex, UnifiedBlock advice) {
+			//a = b;
+			//TODO とりえあずAssignのみ +=,-=などについてはおいおい
+			var assignmentExpressions =
+					root.Descendants<UnifiedBinaryExpression>().Where(e => e.Operator.Kind == UnifiedBinaryOperatorKind.Assign);
+			
+			foreach (var exp in assignmentExpressions) {
+
+				var parent = exp.Parent as UnifiedBlock;
+				var rhs = exp.RightHandSide as UnifiedVariableIdentifier;
+
+				//親がブロック　かつ　右辺がUnifiedVariableIdentifier　でない場合は次の要素へ
+				if(parent == null || rhs == null)
+					continue;
+
+				//変数名が与えられた正規表現にマッチするか確認する
+				var m = regex.Match(rhs.Name);
+				if (!m.Success)
+					continue;
+
+				//アドバイスの合成
+				parent.Insert(parent.IndexOf(exp, 0), advice);
+			}
+
+			//int a = b;
+			var variableDefinitions = root.Descendants<UnifiedVariableDefinition>();
+
+			foreach(var definition in variableDefinitions) {
+				//初期化子を変数として取得
+				var identifier = definition.InitialValue as UnifiedVariableIdentifier;
+				var block = definition.Parent.Parent as UnifiedBlock;
+				
+				//初期化子が変数でない場合は次の要素へ
+				if(block == null || identifier == null)
+					continue;
+
+				//変数名が与えられた正規表現にマッチするか確認する
+				var m = regex.Match(identifier.Name);
+				if (!m.Success)
+					continue;
+
+				//アドバイスの合成
+				var parent = definition.Parent as UnifiedVariableDefinitionList;
+				block.Insert(block.IndexOf(parent, 0), advice);
+			}
+
+		}
+
+		public static void InsertAtAfterGet(IUnifiedElement root, Regex regex, UnifiedBlock advice) {
+			throw new NotImplementedException();
+		}
+
+
+		//setはBinaryExpressionの左辺の場合？
+		// a = b = cの扱いはどうする？
+		public static void InsertAtBeforeSet(IUnifiedElement root, Regex regex, UnifiedBlock advice) {
+			throw new NotImplementedException();
+		}
+
+		public static void InsertAtAfterSet(IUnifiedElement root, Regex regex, UnifiedBlock advice) {
+			throw new NotImplementedException();
+		}
+	}
+}
