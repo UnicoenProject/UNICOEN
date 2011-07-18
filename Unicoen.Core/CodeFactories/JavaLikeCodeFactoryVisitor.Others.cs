bf278baf667718ed916046b1a5dd31db3878a13a
irxground <irxnjhtchlnrw@googlemail.com>
2011-07-19 00:51:36 +0900

    Fix invalid fixture file.

--- Unicoen.Core/CodeFactories/JavaLikeCodeFactoryVisitor.Others.cs
+++ Unicoen.Core/CodeFactories/JavaLikeCodeFactoryVisitor.Others.cs
@@ -182,7 +182,7 @@ namespace Unicoen.CodeFactories {
 
 		public override bool Visit(
 				UnifiedGenericParameter element, VisitorArgument arg) {
-			//element.Modifiers.TryAccept(this, arg);
+			element.Modifiers.TryAccept(this, arg);
 			element.Type.TryAccept(this, arg);
 			element.Constrains.TryAccept(this, arg.Set(AndDelimiter));
 			return false;
