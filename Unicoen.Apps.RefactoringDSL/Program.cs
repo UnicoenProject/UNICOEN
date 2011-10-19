c3194d6d7534c29faad5966f64b0c66543cfe625
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2011-10-20 02:14:05 +0900

    Add and init RefactoringDSL project.

---
+++ Unicoen.Apps.RefactoringDSL/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Unicoen.Apps.RefactoringDSL {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
