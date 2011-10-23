978c90eab9f6b9f621799fcfa04301eedcb88f27
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2011-10-23 17:47:17 +0900

    tmp

---
+++ fixture/Java/input/default/EncapsulateCollection.java
@@ -0,0 +1,11 @@
+class EncapsulateCollection{
+	private int[] _collection;
+	
+	public int[] getCollection(){
+		return _collection;
+	}
+	
+	public void setCollection(int[] collection){
+		_colection = collection;
+	}
+}
