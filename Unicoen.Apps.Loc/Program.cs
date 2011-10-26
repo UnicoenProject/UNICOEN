420375134185c93ef12a9da9f98595491723337a
Kazunori SAKAMOTO <exkazuu@gmail.com>
2011-10-26 14:31:01 +0900

    Create LOC project.

---
+++ Unicoen.Apps.Loc/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Unicoen.Apps.Loc {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
