50731710ad7d5d03e88b3d4a30fb512dc5204dad
Reisha Humaira <reisha.humaira@gmail.com>
2011-11-06 21:23:15 +0900

    Add TLoc.cs to count total lines of code

---
+++ Unicoen.Apps/Loc/Util/TLoc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Unicoen.Apps.Loc.Util
+{
+    class TLoc
+    {
+        public static int CountTLoc(string filePath)
+        {
+            int count = 0;
+            var sr = new StreamReader(filePath);
+            while (sr.ReadLine() != null)
+            {
+                count++;
+            }
+            sr.Close();
+            return count;
+        }
+    }
+}
