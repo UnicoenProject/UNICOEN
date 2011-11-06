e73b81592f5fccd74f098701655789d2099be49e
Reisha Humaira <reisha.humaira@gmail.com>
2011-11-06 21:23:26 +0900

    Add BLoc.cs to count blank lines of code

---
+++ Unicoen.Apps/Loc/Util/BLoc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Unicoen.Apps.Loc.Util
+{
+    class BLoc
+    {
+        public static int CountBLoc(string filePath)
+        {
+            string line;
+            int count = 0;
+            var sr = new StreamReader(filePath);
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line.Trim().Length == 0) count++;
+            }
+            sr.Close();
+            return count;
+        }
+    }
+}
