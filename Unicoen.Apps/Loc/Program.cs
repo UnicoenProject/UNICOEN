375064f7f95477f20872a9f41c7e1e272ea2407b
Reisha Humaira <reisha.humaira@gmail.com>
2011-11-06 21:22:58 +0900

    Add MeasureLoc.cs to measure loc Edit Program.cs to test

--- Unicoen.Apps/Loc/Program.cs
+++ Unicoen.Apps/Loc/Program.cs
@@ -3,18 +3,41 @@ using System.Linq;
 using Unicoen.Languages.Java.ProgramGenerators;
 using Unicoen.Model;
 using Unicoen.Tests;
+using Unicoen.Apps.Loc.Util;
 
 namespace Unicoen.Apps.Loc {
 	class Program {
-		static void Main(string[] args) {
-			var inputPath = FixtureUtil.GetInputPath("Java", "Fibonacci.java");
-			var codeObj = new JavaProgramGenerator().GenerateFromFile(inputPath);
-			var blocks = codeObj.Descendants<UnifiedBlock>();
-			var sum = 0;
-			foreach (var block in blocks) {
-				sum += block.Elements().Count();
-			}
-			Console.WriteLine(sum);
-		}
+		static void Main(string[] args) 
+        {
+            var cInputPath = FixtureUtil.GetInputPath("C", "fibonacci.c");
+            PrintInfo(cInputPath);
+
+            var cSharpInputPath = FixtureUtil.GetInputPath("CSharp", "Student.cs");
+            PrintInfo(cSharpInputPath);
+
+            var javaInputPath = FixtureUtil.GetInputPath("Java", "Fibonacci.java");
+            PrintInfo(javaInputPath);
+
+            var jsInputPath = FixtureUtil.GetInputPath("JavaScript", "student.js");
+            PrintInfo(jsInputPath);
+
+            var pyInputPath = FixtureUtil.GetInputPath("Python2", "fibonacci.py");
+            //PrintInfo(pyInputPath);
+
+            var rubyInputPath = FixtureUtil.GetInputPath("Ruby18", "fibonacci.rb");
+            //PrintInfo(rubyInputPath);
+        }
+
+        static void PrintInfo(string inputPath)
+        {
+            string tloc = "Total Lines of Code: ";
+            string bloc = "Blank Lines of Code: ";
+            string stmt = "Statements Count of Code: ";
+
+            Console.WriteLine("\n" + inputPath);
+            Console.WriteLine(tloc + new MeasureLoc().MeasureTLoc(inputPath));
+            Console.WriteLine(bloc + new MeasureLoc().MeasureBLoc(inputPath));
+            Console.WriteLine(stmt + new MeasureLoc().MeasureStmt(inputPath));
+        }
 	}
 }
---
+++ Unicoen.Apps/Loc/Util/MeasureLoc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Unicoen.Apps.Loc.Util;
+
+namespace Unicoen.Apps.Loc.Util
+{
+    class MeasureLoc
+    {
+        public int MeasureTLoc(string inputPath)
+        {
+            return TLoc.CountTLoc(inputPath);
+        }
+
+        public int MeasureBLoc(string inputPath)
+        {
+            return BLoc.CountBLoc(inputPath);
+        }
+
+        public int MeasureStmt(string inputPath)
+        {
+            var ext = Path.GetExtension(inputPath);
+            switch (ext.ToLower())
+            {
+                case ".c":
+                    return Stmt.StmtC(inputPath);
+                case ".cs":
+                    return Stmt.StmtCSharp(inputPath);
+                case ".java":
+                    return Stmt.StmtJava(inputPath);
+                case ".js":
+                    return Stmt.StmtJavaScript(inputPath);
+                case ".py":
+                    return Stmt.StmtPython(inputPath);
+                case ".rb":
+                    return Stmt.StmtRuby(inputPath);
+                //case ".vb":
+                    //return CSharpFactory.GenerateModel(code);
+            }
+            return 0;
+        }
+    }
+}
