67d310492f8363f342c64fff10f0e4616cef10ad
Reisha Humaira <reisha.humaira@gmail.com>
2011-11-06 21:23:35 +0900

    Add Stmt.cs to count number of statements

---
+++ Unicoen.Apps/Loc/Util/Stmt.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unicoen.Model;
+using Unicoen.Languages.C.ProgramGenerators;
+using Unicoen.Languages.CSharp.ProgramGenerators;
+using Unicoen.Languages.Java.ProgramGenerators;
+using Unicoen.Languages.JavaScript.ProgramGenerators;
+using Unicoen.Languages.Python2.ProgramGenerators;
+using Unicoen.Languages.Python3.ProgramGenerators;
+using Unicoen.Languages.Ruby18.Model;
+
+namespace Unicoen.Apps.Loc.Util
+{
+    class Stmt
+    {
+        public static int StmtC(string inputPath)
+        {
+            var codeObj = new CProgramGenerator().GenerateFromFile(inputPath);
+            //System.IO.File.WriteAllText(@"D:\fibonacci.c.txt", codeObj.ToString());
+            return CountStmt(codeObj);
+        }
+
+        public static int StmtCSharp(string inputPath)
+        {
+            var codeObj = new CSharpProgramGenerator().GenerateFromFile(inputPath);
+            return CountStmt(codeObj);
+        }
+
+        public static int StmtJava(string inputPath)
+        {
+            var codeObj = new JavaProgramGenerator().GenerateFromFile(inputPath);
+            //System.IO.File.WriteAllText(@"D:\Fibonacci.java.txt", codeObj.ToString());
+            return CountStmt(codeObj);
+        }
+
+        public static int StmtJavaScript(string inputPath)
+        {
+            var codeObj = new JavaScriptProgramGenerator().GenerateFromFile(inputPath);
+            return CountStmt(codeObj);
+        }
+
+        public static int StmtPython(string inputPath)
+        {
+            var codeObj = new Python2ProgramGenerator().GenerateFromFile(inputPath);
+            return CountStmt(codeObj);
+        }
+
+        public static int StmtRuby(string inputPath)
+        {
+            var codeObj = new Ruby18ProgramGenerator().GenerateFromFile(inputPath);
+            return CountStmt(codeObj);
+        }
+
+        private static int CountStmt(UnifiedProgram codeObject)
+        {
+            var blocks = codeObject.Descendants<UnifiedBlock>();
+            var sum = 0;
+            foreach (var block in blocks)
+            {
+                sum += block.Elements().Count();
+            }
+            return sum;
+        }
+    }
+}
