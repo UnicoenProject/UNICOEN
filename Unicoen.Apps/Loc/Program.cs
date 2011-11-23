48373bbb2183b4652fb44447b5c282ab65d5e60e
Reisha Humaira <reisha.humaira@gmail.com>
2011-11-23 09:27:22 +0900

    Add foo.java, foo2.java, and point_modified.java for testing

--- Unicoen.Apps/Loc/Program.cs
+++ Unicoen.Apps/Loc/Program.cs
@@ -8,24 +8,51 @@ namespace Unicoen.Apps.Loc {
 	class Program {
 		static void Main(string[] args) 
         {
+            // BASIC MEASUREMENT
             // args[0] is the language
             // arge[1] is the file name or directory name
-            var inputPath = FixtureUtil.GetInputPath(args[0], args[1]);
-            Console.WriteLine("Language : " + args[0]);
-            Console.WriteLine("Input    : " + inputPath + "\n");
-            PrintInfo(inputPath);
+            
+            if (args.Length == 2)
+            {
+                var inputPath = FixtureUtil.GetInputPath(args[0], args[1]);
+                Console.WriteLine("Language : " + args[0]);
+                Console.WriteLine("Input    : " + inputPath + "\n");
+                PrintBasicInfo(inputPath);
+            }
+
+            // DIFFERENTIAL MEASUREMENT
+            // args[0] is the language
+            // arge[1] is the original source code file
+            // arge[2] is the modified source code file
+
+            if (args.Length == 3)
+            {
+                var orign = FixtureUtil.GetInputPath(args[0], args[1]);
+                var modif = FixtureUtil.GetInputPath(args[0], args[2]);
+                Console.WriteLine("Language      : " + args[0]);
+                Console.WriteLine("Original File : " + orign);
+                Console.WriteLine("Modified File : " + modif + "\n");
+                PrintDifferentialInfo(orign, modif);
+            }
         }
 
-        static void PrintInfo(string inputPath)
+        static void PrintBasicInfo(string inputPath)
         {
-            string tloc = "Total Lines of Code: ";
-            string bloc = "Blank Lines of Code: ";
-            string stmt = "Statements Count of Code: ";
-
-            Console.WriteLine(tloc + TotalLoc.CountTotalLoc(inputPath));
-            Console.WriteLine(bloc + BlankLoc.CountBlankLoc(inputPath));
-            Console.WriteLine(stmt + StatementLoc.CountStatementLoc(inputPath));
+            Console.WriteLine("Total Lines of Code      : " + TotalLoc.CountTotalLoc(inputPath));
+            Console.WriteLine("Blank Lines of Code      : " + BlankLoc.CountBlankLoc(inputPath));
+            Console.WriteLine("Statements Count of Code : " + StatementLoc.CountStatementLoc(inputPath));
             Console.WriteLine();
         }
+
+        static void PrintDifferentialInfo(string orign, string modif)
+        {
+            DiffCounter c = DifferentialLoc.CountDifferentialLoc(orign, modif);
+            Console.WriteLine("Added Line of Code    : " + c.NumAdded);
+            Console.WriteLine("Deleted Line of Code  : " + c.NumDeleted);
+            Console.WriteLine("Modified Line of Code : " + c.NumModified);
+            Console.WriteLine("Equal Line of Code    : " + c.NumEqual);
+            Console.WriteLine();
+        }
+
 	}
 }
---
+++ fixture/Java/input/foo.java
@@ -0,0 +1,9 @@
+public class foo {
+	public static int foo(int n) {
+		if (n < 2) {
+			return n;
+		}
+	}
+	public static void Main(string[] args)
+	}
+}
