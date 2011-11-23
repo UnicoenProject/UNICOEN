c3f94580248af777ed58c7b825c52db571423bf2
Reisha Humaira <reisha.humaira@gmail.com>
2011-11-23 09:27:57 +0900

    Add DiffCounter.cs and DifferentialLoc.cs for differential measurement

---
+++ Unicoen.Apps/Loc/Util/DiffCounter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Unicoen.Apps.Loc.Util
+{
+    // class containing number of differential elements
+    public class DiffCounter
+    {
+        public int NumAdded { get; set; }
+        public int NumDeleted { get; set; }
+        public int NumModified { get; set; }
+        public int NumEqual { get; set; }
+
+        public DiffCounter(int na, int nd, int nm, int ne)
+        {
+            NumAdded = na;
+            NumDeleted = nd;
+            NumModified = nm;
+            NumEqual = ne;
+        }
+
+        public DiffCounter()
+            : this(0, 0, 0, 0)
+        {
+        }
+    }
+}
---
+++ Unicoen.Apps/Loc/Util/DifferentialLoc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using DiffPlex;
+using DiffPlex.DiffBuilder;
+using DiffPlex.DiffBuilder.Model;
+
+
+namespace Unicoen.Apps.Loc.Util
+{
+    public class DifferentialLoc
+    {
+        // measure differential between two files
+        public static DiffCounter CountDifferentialLoc(string originalFile, string modifiedFile)
+        {
+            string original = File.ReadAllText(@originalFile);
+            string modified = File.ReadAllText(@modifiedFile);
+            
+            var d = new Differ();
+            var counter = new DiffCounter();
+            var side = new SideBySideDiffBuilder(d);
+            var result = side.BuildDiffModel(original, modified);
+
+            // added, modified, and unmodified at modified file
+            foreach (var line in result.NewText.Lines)
+            {
+                if (line.Type == ChangeType.Inserted)  counter.NumAdded++;
+                if (line.Type == ChangeType.Modified)  counter.NumModified++;
+                if (line.Type == ChangeType.Unchanged) counter.NumEqual++;
+            }
+
+            // deleted from original file
+            foreach (var line in result.OldText.Lines)
+            {
+                if (line.Type == ChangeType.Deleted)   counter.NumDeleted++;
+            }
+
+            return counter;
+        }
+    }
+}
