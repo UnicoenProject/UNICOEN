ed12bb257da6b2c4fe1682bf5cb8b39eba0335dd
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2011-11-28 02:17:21 +0900

    Init NamespaceDetector.

---
+++ Unicoen.Apps/RefactoringDSL/NamespaceDetector/Namespace.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Unicoen.Apps.RefactoringDSL.NamespaceDetector {
+	public class Namespace {
+		public string Name { set; get; }
+		public Type ElementType { set; get; }
+	}
+}
