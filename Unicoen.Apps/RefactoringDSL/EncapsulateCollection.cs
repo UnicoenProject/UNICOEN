62e6e3a29126843be38e96c52e48c261dbb9a0c2
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2011-11-28 19:03:47 +0900

    Modify and refactor EncapsulateCollection.

--- Unicoen.Apps/RefactoringDSL/EncapsulateCollection.cs
+++ Unicoen.Apps/RefactoringDSL/EncapsulateCollection.cs
@@ -2,9 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Unicoen.Apps.RefactoringDSL.Java;
+using Unicoen.Apps.RefactoringDSL.Util;
+using Unicoen.Model;
 
 namespace Unicoen.Apps.RefactoringDSL {
-	public class EncapsulateCollection {
+	public class EncapsulateCollection{
+		private UnifiedProgram Program { get; set; }
 
+		// コンストラクタ
+		public EncapsulateCollection(UnifiedProgram program) {
+			this.Program = program;
+		}
+
+		public UnifiedProgram Refactor(string className) {
+			var model = Program.DeepCopy();
+			var targetClass = FindUtil.FindClassByClassName(model, className).First();
+			var genericParameters = FindUtil.SearchGenericsField(model, "List");
+
+			var list = targetClass.Body.DeepCopy();
+			foreach (var gp in genericParameters) {
+				var addingProcedure = EncapsulateCollectionHelperForJava.GenerateAddingProcedureForList((UnifiedVariableDefinition)gp);
+				var removingProcedure = EncapsulateCollectionHelperForJava.GenerateRemovingProcedureForList((UnifiedVariableDefinition)gp);
+
+				var addMethod = EncapsulateCollectionHelper.GenerateAddMethod(gp, "addItem", addingProcedure);
+				var removeMethod = EncapsulateCollectionHelper.GenerateRemoveMethod(gp, "removeItem", removingProcedure);
+				var collectionFieldGetter = EncapsulateCollectionHelper.GenerateClonedFieldGetter((UnifiedVariableDefinition)gp);
+
+				list.Add(addMethod);
+				list.Add(removeMethod);
+				list.Add(collectionFieldGetter);
+			}
+
+			targetClass.Body = list;
+
+			return model;
+
+		}
 	}
 }
