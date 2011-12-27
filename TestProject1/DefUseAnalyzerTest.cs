7b37e56de77a3e6fb0194c24f61fe7e99d2cdbf2
Satoshi Arai <st.arai0227@gmail.com>
2011-12-27 21:58:30 +0900

    Implementing Def-Use Analyzer

---
+++ TestProject1/DefUseAnalyzerTest.cs
@@ -0,0 +1,89 @@
+﻿using Unicoen.Apps.Findbug;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Unicoen.Model;
+using System.Collections.Generic;
+
+namespace TestProject1
+{
+    
+    
+    /// <summary>
+    ///DefUseAnalyzerTest のテスト クラスです。すべての
+    ///DefUseAnalyzerTest 単体テストをここに含めます
+    ///</summary>
+    [TestClass()]
+    public class DefUseAnalyzerTest {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///現在のテストの実行についての情報および機能を
+        ///提供するテスト コンテキストを取得または設定します。
+        ///</summary>
+        public TestContext TestContext {
+            get {
+                return testContextInstance;
+            }
+            set {
+                testContextInstance = value;
+            }
+        }
+
+        #region 追加のテスト属性
+        // 
+        //テストを作成するときに、次の追加属性を使用することができます:
+        //
+        //クラスの最初のテストを実行する前にコードを実行するには、ClassInitialize を使用
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //クラスのすべてのテストを実行した後にコードを実行するには、ClassCleanup を使用
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //各テストを実行する前にコードを実行するには、TestInitialize を使用
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //各テストを実行した後にコードを実行するには、TestCleanup を使用
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///FindDefines のテスト
+        ///</summary>
+        [TestMethod()]
+        public void FindDefinesTest() {
+            IEnumerable<IUnifiedElement> expected = null; // TODO: 適切な値に初期化してください
+            IEnumerable<IUnifiedElement> actual;
+            actual = DefUseAnalyzer.FindDefines();
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("このテストメソッドの正確性を確認します。");
+        }
+
+        /// <summary>
+        ///FindUses のテスト
+        ///</summary>
+        [TestMethod()]
+        public void FindUsesTest() {
+            IEnumerable<IUnifiedElement> expected = null; // TODO: 適切な値に初期化してください
+            IEnumerable<IUnifiedElement> actual;
+            actual = DefUseAnalyzer.FindUses();
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("このテストメソッドの正確性を確認します。");
+        }
+    }
+}
---
+++ TestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットを通して制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("TestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TestProject1")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから
+// 参照できなくなります。このアセンブリ内で COM から型にアクセスする必要がある場合は、 
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+[assembly: Guid("46f17e33-9214-4f5c-b42f-2ae6528da25a")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、以下のように '*' を使用してビルド番号とリビジョン番号を 
+// 既定値にすることができます:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
