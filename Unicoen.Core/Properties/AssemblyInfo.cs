a2e7a44e9c9a981828c0949d5e33a4f6e9cbb4a8
Kazunori SAKAMOTO <exkazuu@gmail.com>
2012-01-23 15:21:16 +0900

    Release 1.0.3.28.

--- Unicoen.Core/Properties/AssemblyInfo.cs
+++ Unicoen.Core/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.3.27")]
-[assembly: AssemblyFileVersion("1.0.3.27")]
+[assembly: AssemblyVersion("1.0.3.28")]
+[assembly: AssemblyFileVersion("1.0.3.28")]
