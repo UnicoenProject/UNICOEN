0f807167d7af5c0f5730fdacde6e63dce1e7aa9b
SAKAMOTO Kazunori <exkazuu@gmail.com>
2012-02-24 11:19:40 +0900

    Remove NRefactory dlls from lib directory. Readd NRefactory with NuGet.

--- Unicoen.Core/Properties/AssemblyInfo.cs
+++ Unicoen.Core/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.3.28")]
-[assembly: AssemblyFileVersion("1.0.3.28")]
+[assembly: AssemblyVersion("1.0.3.29")]
+[assembly: AssemblyFileVersion("1.0.3.29")]
