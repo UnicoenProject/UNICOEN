aded1424a54ccff526a58403ae1b3dbc40e01abd
Kazunori Sakamoto <exkazuu@gmail.com>
2012-03-22 16:40:50 +0900

    Prepare to release new version.

--- Unicoen.Apps/Metrics/Program.cs
+++ Unicoen.Apps/Metrics/Program.cs
@@ -1,6 +1,6 @@
 ﻿#region License
 
-// Copyright (C) 2011 The Unicoen Project
+// Copyright (C) 2011-2012 The Unicoen Project
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -26,8 +26,8 @@ namespace Unicoen.Apps.Metrics {
         private const int W = 12;
 
         private static readonly string Usage =
-                "Unicoen.Applications.Metrics 1.0.0" + "\n" +
-                "Copyright (C) 2011 Kazunori SAKAMOTO" + "\n" +
+                "Unicoen.Applications.Metrics 1.0.1" + "\n" +
+                "Copyright (C) 2011-2012 SAKAMOTO Kazunori" + "\n" +
                 "" + "\n" +
                 "Usage: Metrics <command> [args]" + "\n" +
                 "" + "\n" +
