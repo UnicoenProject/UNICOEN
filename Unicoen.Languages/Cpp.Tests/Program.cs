d0600372f1bbde5ee845aa2f34eb0e0117ab0d95
Kazunori Sakamoto <exkazuu@gmail.com>
2013-02-06 19:22:33 +0900

    Move some projects.

---
+++ Unicoen.Languages/Cpp.Tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Unicoen.Languages.Cpp.Tests
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ Unicoen.Languages/Cpp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Unicoen.Languages.Cpp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
